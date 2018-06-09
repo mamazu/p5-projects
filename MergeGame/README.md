879270dbe0c8936c6196be7fc737d3be76b9e2be
mamazu <maxo.pech@gmail.com>
2018-06-09 14:36:54 +0200

    Added the merge game to the repo

---
+++ MergeGame/README.md
@@ -0,0 +1,2 @@
+# Merge game
+A simple game where you merge two circles you get an upgrade.
---
+++ MergeGame/sketch.js
@@ -0,0 +1,43 @@
+const speed = 2000;
+const circles = new CircleList();
+let selectedCircle = null;
+
+function setup() {
+  let canvas = createCanvas(600, 600);
+  ellipseMode(RADIUS);
+  textAlign(CENTER, CENTER);
+  frameRate(20);
+
+  setInterval(c => circles.addCircle(), speed);
+  canvas.mouseMoved(() => {
+    if (selectedCircle !== null) {
+      selectedCircle.position = createVector(mouseX, mouseY);
+    }
+  })
+}
+
+function draw() {
+  background(0);
+
+  circles.update();
+  circles.draw();
+
+  textSize(45);
+  fill(255, 0, 255);
+  text(circles.getValue(), 45, 45);
+}
+
+function mousePressed() {
+  selectedCircle = circles.getCircleAt(createVector(mouseX, mouseY));
+  // circles.sortToFront(selectedCircle);
+}
+
+function mouseReleased() {
+  selectedCircle = null;
+}
+
+function keyPressed(e) {
+  if (e.keyCode === 77) {
+    circles.magnetize();
+  }
+}
